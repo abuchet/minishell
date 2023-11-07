bcf2cac5ac1b3e32544f4020d1fe1a581571e894
abuchet <110593232+abuchet@users.noreply.github.com>
2023-11-07 12:26:23 +0100

    Add files via upload

---
+++ minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abuchet <abuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 15:41:16 by abuchet           #+#    #+#             */
+/*   Updated: 2023/10/13 15:48:38 by abuchet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#endif
